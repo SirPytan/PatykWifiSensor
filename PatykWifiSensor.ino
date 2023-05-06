0f810cb300774d4a3d5904c507d7555865f79731
Daniel P <daniel.patyk@gmx.de>
2023-05-06 22:32:38 +0200

    Initial Commit

---
+++ PatykWifiSensor.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		PatykWifiSensor.ino
+ Created:	6th May 2023
+ Author:	Daniel Patyk
+*/
+
+#include <WiFiNINA.h>
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
